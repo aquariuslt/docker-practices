83d3d5f61f44b205bef73a806d716e7640a53d91
Jason Cui <jason.cui@oocl.com>
2018-02-22 14:24:01 +0800

    [Weblogic] Create ext image for installing zip/unzip

---
+++ images/weblogic/ext/build-weblogic-ext-docker-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Author: superaquariuslt@gmail.com
+# Build weblogic 12.2.1.3 base on oracle jre:8 environment with some daily features
+# - zip/unzip
+
+docker build . -t aquariuslt/weblogic:12.2.1.2-ext
